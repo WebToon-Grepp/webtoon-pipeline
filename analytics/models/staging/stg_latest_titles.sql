8b73546378719ab5b95fba8bd5da6396cc52b01d
Rooam Lee <leewr9@naver.com>
2025-03-06 19:16:43 +0900

    analytics:: dbt staging 모델 추가
    임시 View 모델

---
+++ analytics/models/staging/stg_latest_titles.sql
@@ -0,0 +1,14 @@
+WITH titles AS (
+
+    SELECT 
+        *, 
+       ROW_NUMBER() OVER (PARTITION BY id, release_day ORDER BY views DESC) AS title_no
+    FROM raw_data.titles e
+
+)
+
+SELECT 
+    *
+FROM titles
+WHERE title_no = 1
+
