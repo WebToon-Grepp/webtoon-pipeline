6ee9d7d70f9d17cedff88ac052ef557007e37c7b
Rooam Lee <leewr9@naver.com>
2025-03-09 19:36:21 +0900

    analytics:: 타이틀/장르 테이블 조인

---
+++ analytics/models/site/dim_webtoon_titles.sql
@@ -0,0 +1,22 @@
+WITH join_titles_genres AS (
+
+    SELECT 
+        tt.platform, 
+        tt.id, 
+        tt.title, 
+        tt.author, 
+        tt.image_url, 
+        tt.views, 
+        tt.likes, 
+        tt.comments, 
+        tt.is_completed, 
+        g.genre_name
+    FROM raw_data.genres g
+    LEFT JOIN {{ ref('stg_total_titles') }} tt
+        ON g.title_id = tt.id 
+        AND g.platform = tt.platform 
+)
+
+SELECT DISTINCT 
+    *
+FROM join_titles_genres
