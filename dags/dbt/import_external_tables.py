8e9d4ea09a47ea1592072c07b9f79070e50adc0a
Rooam Lee <leewr9@naver.com>
2025-03-11 11:37:21 +0900

    dags::dbt:: sensor 타겟 변경
    execution_date 기준으로 동작하여 트리거로 동작하던 기존 dag 시간을 맞출 수가 없음

--- dags/dbt/import_external_tables.py
+++ dags/dbt/import_external_tables.py
@@ -25,18 +25,24 @@ with DAG(
     
     wait_for_naver_sensor = ExternalTaskSensor(
         task_id="wait_for_naver",
-        external_dag_id="load_naver_daily_data",
+        external_dag_id="fetch_and_store_naver_daily_data",
+        external_task_id="trigger_optimize",
         mode="reschedule",
         timeout=2500,
-        poke_interval=500
+        poke_interval=500,
+        allowed_states=["success"],
+        failed_states=["failed", "skipped"]
     )
 
     wait_for_kakao_sensor = ExternalTaskSensor(
         task_id="wait_for_kakao",
-        external_dag_id="load_kakao_daily_data",
+        external_dag_id="fetch_and_store_kakao_daily_data",
+        external_task_id="trigger_optimize",
         mode="reschedule",
         timeout=2500,
-        poke_interval=500
+        poke_interval=500,
+        allowed_states=["success"],
+        failed_states=["failed", "skipped"]
     )
     
     run_dbt_model_task = BashOperator(
@@ -58,13 +64,17 @@ with DAG(
     trigger_dashboard_task = TriggerDagRunOperator(
         task_id="trigger_dashboard",
         trigger_dag_id="dashboard_data_transform",
-        wait_for_completion=False
+        wait_for_completion=True,
+        poke_interval=100,
+        deferrable=True
     )
 
     trigger_site_task = TriggerDagRunOperator(
         task_id="trigger_site",
         trigger_dag_id="site_data_transform",
-        wait_for_completion=False
+        wait_for_completion=True,
+        poke_interval=100,
+        deferrable=True
     )
 
     [wait_for_naver_sensor, wait_for_kakao_sensor] >> run_dbt_model_task >> [trigger_dashboard_task, trigger_site_task]
