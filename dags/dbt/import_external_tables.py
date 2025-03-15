dadcccb513f6ce69a3f6ec5923fbab5aca33e0a3
Rooam Lee <leewr9@naver.com>
2025-03-15 10:27:00 +0900

    dags:: dbt 환경변수 롤백

--- dags/dbt/import_external_tables.py
+++ dags/dbt/import_external_tables.py
@@ -25,6 +25,11 @@ with DAG(
     run_dbt_import_task = BashOperator(
         task_id="run_dbt_import",
         env={
+            "DBT_DBNAME": Variable.get("DBT_DBNAME"),
+            "DBT_HOST": Variable.get("DBT_HOST"),
+            "DBT_PASSWORD": Variable.get("DBT_PASSWORD"),
+            "DBT_SCHEMA": Variable.get("DBT_SCHEMA"),
+            "DBT_USER": Variable.get("DBT_USER")
         },
         bash_command=f"""
             cd {DBT_PROJECT_DIR} &&
@@ -36,6 +41,11 @@ with DAG(
     run_dbt_staging_task = BashOperator(
         task_id="run_dbt_staging",
         env={
+            "DBT_DBNAME": Variable.get("DBT_DBNAME"),
+            "DBT_HOST": Variable.get("DBT_HOST"),
+            "DBT_PASSWORD": Variable.get("DBT_PASSWORD"),
+            "DBT_SCHEMA": Variable.get("DBT_SCHEMA"),
+            "DBT_USER": Variable.get("DBT_USER")
         },
         bash_command=f"""
             cd {DBT_PROJECT_DIR} &&
